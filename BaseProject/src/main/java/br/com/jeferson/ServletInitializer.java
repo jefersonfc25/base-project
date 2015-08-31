730f761dbb6ad60a45d20e8bb4a717f4f1872102
jeferson <jefersonfc25@gmail.com>
2015-08-31 16:55:17 -0300

    Import inicial

---
+++ BaseProject/src/main/java/br/com/jeferson/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.jeferson;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BaseProjectApplication.class);
+	}
+
+}
